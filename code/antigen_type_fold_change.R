552a0a21b6d842f120b5aa75da032685e74b5715
Antonia Netzl <antonia.netzl@gmail.com>
2022-03-23 13:04:31 +0000

    Fold change from homologous plot

--- code/antigen_type_fold_change.R
+++ code/antigen_type_fold_change.R
@@ -5,6 +5,7 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 library(Racmacs)
+library(patchwork)
 
 mapValues <- function(val_fn, name_fn) {
   function(map) {
@@ -41,6 +42,7 @@ calc_fold_changes <- function(map, titer_table) {
     )
     
     for (ag in seq_len(numAntigens(map))) {
+      
       homologous_titers <- titer_table[homo_ag, sr]
       ag_titers <- titer_table[ag, sr]
       titer_diff_est <- meantiter::mean_titer_diffs(
@@ -71,21 +73,27 @@ calc_fold_changes <- function(map, titer_table) {
     
   }
   
+  
   # Remove NA diffs
-  all_group_results %>% 
-    filter(
-      !is.na(diff)
-    ) -> all_group_results
+   all_group_results %>% 
+     filter(
+       !is.na(diff)
+     ) -> all_group_results
   
   return(all_group_results)
   
 }
 
-
 map_lv<- read.acmap("./data/map/omicron_neut_LV_map.ace")
 map_pv<- read.acmap("./data/map/omicron_neut_PV_map.ace")
 
 agFillValues <- mapValues(agFill, agNames)
+srGroupValues <- mapValues(srOutline, srGroups)
+
+#View(titerTable(map_pv)[,srGroups(map_pv)=="3x Vax"])
+# sr colors
+#sr_colors <- unique(srOutline(map_lv))
+#names(sr_colors) <- unique(as.character(srGroups(map_lv)))
 
 homologous_ags <- c(
   "3x Vax" = "WT",
@@ -115,10 +123,7 @@ foldchange <- \(x) {
   
 }
 
-plots <- list()
-for (sr_group_name in unique(combo$sr_group)) {
-  
-  
+do_fold_change_plot <- function(combo, sr_group_name) {
   combo %>%
     filter(
       sr_group == sr_group_name
@@ -127,26 +132,31 @@ for (sr_group_name in unique(combo$sr_group)) {
   temp_ag_levels <- unique(sr_group_results$ag[order(-sr_group_results$diff)])
   
   sr_group_results$x <- unlist(lapply(sr_group_results$ag, function(x) grep(x, temp_ag_levels)))
+  
   sr_group_results %>%
     ggplot(
       aes(
-        x = ifelse(`Antigen type` == "Live-virus", x - 0.15, x + 0.15),
+        x = x,#ifelse(`Antigen type` == "Live-virus", x - 0.15, x + 0.15),
         y = diff,
         ymin = diff_lower,
         ymax = diff_upper,
-        color = ag,
-        shape = `Antigen type`
+        shape = `Antigen type`,
+        group = `Antigen type`
       )
     ) + 
+    geom_line(aes(color = sr_group,
+                  linetype = `Antigen type`)) +
     geom_pointrange(
-      show.legend = FALSE
+      aes(color = ag),
+      show.legend = FALSE,
+      size = 0.5
     ) +
     scale_x_continuous(
       breaks = 1:length(temp_ag_levels),
       labels = temp_ag_levels
     ) + 
     scale_color_manual(
-      values = agFillValues(map_lv)
+      values = c(agFillValues(map_lv), srGroupValues(map_lv))
     ) + 
     scale_y_continuous(
       breaks = 2:min(floor(combo$diff_lower), na.rm=T),
@@ -171,35 +181,120 @@ for (sr_group_name in unique(combo$sr_group)) {
         angle = 45,
         hjust = 1
       ),
-      panel.grid.minor = element_blank()
+      axis.title.y = element_text(
+        size = 8
+      ),
+      panel.grid.minor = element_blank(),
+      legend.position = "none"
     ) -> gp
   
   gp <- gp + 
     geom_text(data = sr_group_results %>% filter(`Antigen type` == "Live-virus"),
               aes(
-                x = x - 0.2,
+                x = x,
                 y = 2,
                 label = paste0("LV: ",foldchange(diff))
               ),
-              size = 2.5,
+              size = 2,
               color = "grey20"
     ) +
     geom_text(data = sr_group_results %>% filter(`Antigen type` == "Pseudovirus"),
               aes(
-                x = x + 0.2,
-                y = 2,
+                x = x,
+                y = 2-0.4,
                 label = paste0("PV: ",foldchange(diff))
               ),
-              size = 2.5,
+              size = 2,
               color = "grey20"
     )
   
-  # Output the plot
-  num_ags <- length(unique(sr_group_results$ag))
-  plots <- c(plots, list(gp))
+  return(gp)
+}
+
+do_ratio_plot <- function(combo, sr_group_name) {
+  combo %>%
+    filter(
+      sr_group == sr_group_name
+    ) %>%
+    mutate(fc = as.numeric(foldchange(diff)))-> sr_group_results
   
+  temp_ag_levels <- unique(sr_group_results$ag[order(-sr_group_results$diff)])
+  
+  sr_group_results$x <- unlist(lapply(sr_group_results$ag, function(x) grep(x, temp_ag_levels)))
+  
+  sr_group_results %>%
+    select(sr_group, ag, `Antigen type`, fc, x) %>%
+    pivot_wider(names_from = `Antigen type`, values_from = fc) %>%
+    mutate(ratio_full = `Live-virus`/Pseudovirus,
+           ratio = log2(abs(ratio_full))) -> ratio_df
+          # ratio = ifelse(ratio_full < 0, -ratio, ratio)) -> ratio_df
+  
+  ratio_df %>%
+    ggplot(
+      aes(
+        x = x,#ifelse(`Antigen type` == "Live-virus", x - 0.15, x + 0.15),
+        y = ratio
+      )
+    ) + 
+    geom_line(aes(color = sr_group)) +
+    geom_point(
+      aes(color = ag),
+      show.legend = FALSE,
+      size = 2
+    ) +
+    scale_x_continuous(
+      breaks = 1:length(temp_ag_levels),
+      labels = temp_ag_levels
+    ) + 
+    scale_color_manual(
+      values = c(agFillValues(map_lv), srGroupValues(map_lv))
+    ) + 
+    scale_y_continuous(
+      labels = function(x) round(2^x,1),
+      breaks = seq(-2.5,2.5,0.5),
+      limits = c(-2.7,2.7)
+    ) +
+    geom_hline(
+      yintercept = 0,
+      linetype = "dashed",
+      color = "grey40"
+    ) + 
+    labs(
+      x = "",
+      y = "Fold drop LV/PV"
+    ) + 
+    theme_bw() +
+    theme(
+      axis.text.x = element_text(
+        angle = 45,
+        hjust = 1
+      ),
+      axis.title.y = element_text(
+        size = 8
+      ),
+      panel.grid.minor = element_blank(),
+      legend.position = "none"
+    ) -> gp_ratio
+  
+  return(gp_ratio)
   
 }
 
-plots
+plots <- list()
+for (sr_group_name in unique(combo$sr_group)) {
+  
+  gp <- do_fold_change_plot(combo, sr_group_name)
+  gp_diff <- do_ratio_plot(combo, sr_group_name)
+  
+  combo_plot <- (gp + theme(axis.title.x=element_blank(),
+                                       axis.text.x=element_blank(),
+                                       axis.ticks.x=element_blank())) /gp_diff +
+    plot_layout(heights = c(1.5, 1)) 
+
+  plots <- c(plots, list(combo_plot))
+  
+}
+
+ggpubr::ggarrange(plotlist = plots, labels = c("A", "B", "C", "D", "E", "F", "G")) -> all_plots
+ggsave("./figures/fold_change_from_homologous.png", plot = all_plots, width = 12, height = 14)
 
